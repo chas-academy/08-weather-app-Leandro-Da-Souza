65c06797978d55173d7e4ef17df9d1c42a7675f8
Leandro Da Souza <leandrodasouza@Leandros-MacBook-Pro.local>
2019-03-19 13:43:27 +0100

    create components folder and initilize components with basic markup, export and import them to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Body from './components/Body';
+import Form from './components/Form';
+import Header from './components/Header';
+import ApiCall from './components/ApiCall';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>IT'S ALIVE</h1>
-        <p>“Nothing is so painful to the human mind as a great and sudden change.” </p>
+        <Header />
+        <ApiCall />
+        <Form/>
+        <Body />
       </div>
       
     );
---
+++ src/components/ApiCall.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ApiCall extends Component {
+    render(){
+        return(
+            <div>API CALL</div>
+        )
+    }
+}
+
+export default ApiCall
